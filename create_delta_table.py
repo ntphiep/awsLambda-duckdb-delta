4e4c35d9c3c92ca18330081a853d2487de0e08cf
Nguyen Hiep <nguyen.tph128@gmail.com>
2025-01-01 17:18:19 +0700

    feat: update using polars instead of daft

--- create_delta_table.py
+++ create_delta_table.py
@@ -1,16 +1,24 @@
-import daft
+import polars as pl
 
-df = daft.read_csv("2024-07-01.csv")
+df = pl.read_csv("2024-07-01.csv")
 
 columns = ['date','serial_number','model','capacity_bytes','failure','datacenter','cluster_id','vault_id','pod_id','pod_slot_num']
 
 df_cleaned = df.select(*columns)
-# df_cleaned.write_deltalake("s3://hiep-delta-bk/ducklamb")
+df_cleaned.write_delta(
+    "s3://hiep-delta-bk/ducklamb",
+    storage_options={
+        "AWS_S3_ALLOW_UNSAFE_RENAME": "true"
+    })
 
-# df = daft.from_pydict({
-#     "date": ['2024-12-30'], 
-#     'model': ['ST4000DM000'], 
-#     'failure_rate': [0]
-# })
+df = pl.DataFrame({
+    "date": ['2024-12-30'], 
+    "model": ['ST4000DM000'], 
+    "failure_rate": [0]
+})
 
-# df.write_deltalake("s3://hiep-delta-bk/ducklambcummulative", partition_cols=['date'])
+df.write_deltalake(
+    "s3://hiep-delta-bk/ducklambcummulative", 
+    storage_options={
+        "AWS_S3_ALLOW_UNSAFE_RENAME": "true"
+    })
